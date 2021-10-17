923a55e5359328cee07231f3164542265b273dcb
RY2291 <prgr2498@gmail.com>
2021-10-17 07:29:28 +0000

    [add]association, validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,17 @@
 class Customer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :addresses, dependent: :destroy
+  has_many :orders
+  has_many :cartitems    
+  
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :phone_number, presence: true, length: { in: 10..11 }
+  validates :address, presence: true
 end
+
