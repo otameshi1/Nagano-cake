7df353055be24e00626f476558a75d732ef55f18
umi <yana3umi@gmil.com>
2021-10-17 02:38:42 +0000

    [Add]admin_devise,order_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_015525) do
+ActiveRecord::Schema.define(version: 2021_10_17_022744) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2021_10_17_015525) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "postage"
+    t.integer "customer_id"
+    t.string "addresses_name"
+    t.string "postal_code"
+    t.integer "total_payment"
+    t.integer "order_status"
+    t.integer "payment_method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
