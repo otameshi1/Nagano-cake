1c2424261a680a85669bef3772cc88ca9cf6a64d
yanagiya-umi(imac) <yana3umi@gmail.com>
2021-10-19 16:55:09 +0900

    teset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
 
     def after_sign_in_path_for(resource)
-        admin_products_path
+        products_path
     end
 
     def after_sign_up_path_for(resource)
-        root_path
+        products_path
     end
 
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,12 +6,6 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :cartitems    
   
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :postal_code, presence: true
-  validates :phone_number, presence: true, length: { in: 10..11 }
-  validates :address, presence: true
+
 end
 
