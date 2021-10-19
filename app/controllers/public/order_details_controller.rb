6120843821deb8d1c9aca05dd079ea2495f43de5
sakuow <yusaku1335@gmail.com>
2021-10-19 19:17:16 +0900

    view4-4 commit

--- app/controllers/public/order_details_controller.rb
+++ app/controllers/public/order_details_controller.rb
@@ -1,2 +1,11 @@
 class Public::OrderDetailsController < ApplicationController
+
+  def index
+    @order_details = OrderDetails.all
+  end
+
+  def show
+    @order_detail = OrderDetail.find(params[:id])
+  end
+
 end
