be6a315cca35b2b1aec0d36c1b84dd2fbacf3e97
siyo0715 <hms0175km@gmail.com>
2021-10-20 09:13:29 +0000

    detail

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2021_10_19_065413) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
@@ -54,8 +56,6 @@ ActiveRecord::Schema.define(version: 2021_10_19_065413) do
     t.string "postal_code"
     t.string "address"
     t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "is_deleted"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_065413) do
     t.text "introduction"
     t.string "image_id"
     t.integer "price"
-    t.boolean "is_sale", default: true
+    t.boolean "ia_sale", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
